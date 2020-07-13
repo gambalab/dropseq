83d6af145fce579b87b10590c75d76c47c1be7a4
jeky82 <gambardella@tigem.it>
2020-07-13 17:07:32 +0200

    empty drop support

---
+++ emptyDrop.R
@@ -0,0 +1,64 @@
+library(DropletUtils)
+library(Matrix)
+
+###########################################################################################################
+# Ribo Mito Genes (Code Snippet)
+#
+#
+#library(biomaRt)
+#ensembl = biomaRt::useMart("ensembl",dataset="hsapiens_gene_ensembl")
+#all = biomaRt::getBM( attributes=c("ensembl_gene_id", "hgnc_symbol", "gene_biotype"), mart = ensembl)
+#ix <- grepl(pattern = "^RPS", x = all$hgnc_symbol)
+#ix <- ix | grepl(pattern = "^RPL", x = all$hgnc_symbol)
+#ix <- ix | grepl(pattern = "^MT-", x = all$hgnc_symbol)
+#ix <- ix | all$gene_biotype%in%c("rRNA","Mt_rRNA","Mt_tRNA")
+#all = all[ix,]
+#saveRDS(all,"~/dropseq_git/data/ribo.mito.genes.rds")
+#
+###########################################################################################################
+
+rmdublecells = function(M) {cellSum <- Matrix::colSums(M);M[,cellSum < quantile(cellSum,.75) + 3*( quantile(cellSum,.75) - quantile(cellSum,.25))]}
+
+
+# Input Parametres
+# ----------------
+args <- commandArgs(trailingOnly = TRUE)
+wd = as.character(args[1])
+repoDir=as.character(args[2])
+sample=as.character(args[3])
+cpus=as.numeric(args[4])
+
+ribomt = readRDS(paste0(repoDir,"/data/ribo.mito.genes.rds"))
+dir.create(paste0(wd,"/RData"),showWarnings = F)
+
+M = Matrix::readMM(paste0(wd,"/Gene/raw/matrix.mtx"))
+bc = read.delim(paste0(wd,"/Gene/raw/barcodes.tsv"),stringsAsFactors = F,header = F)
+colnames(bc) = "barcode"
+colnames(M) = bc$barcode
+genes = read.delim(paste0(wd,"/Gene/raw/features.tsv"),stringsAsFactors = F,header = F)
+rownames(M) = substr(x = genes$V1,start = 1,stop = 15)
+
+
+# Run emptyDrops excluding mito/ribo
+set.seed(100)
+keep = !rownames(M)%in%ribomt$ensembl_gene_id
+e.keep <- DropletUtils::emptyDrops(M[keep, ], retain = Inf,BPPARAM = BiocParallel::MulticoreParam(workers = cpus))
+summary(e.keep$FDR < 0.01)
+
+is.cell <- e.keep$FDR <= 0.01
+plot(e.keep$Total, -e.keep$LogProb, col=ifelse(is.cell, "red", "black"),xlab="Total UMI count", ylab="-Log Probability")
+
+is.cell[is.na(is.cell)] = FALSE
+M = M[,is.cell]
+
+# Remove putative doublets
+M = rmdublecells(M)
+
+# Remove low covarage cells
+M = M[,Matrix::colSums(M)>=500]
+
+# Cut and Save results
+M = M[Matrix::rowSums(M)>0,]
+saveRDS(M,paste0(wd,"/RData/",sample,"Filtered.mtx.rds"))
+
+        
